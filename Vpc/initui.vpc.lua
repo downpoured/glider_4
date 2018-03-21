9c35f76ba149ef21cfad5c6a8eceffc2cb507bf8
Ben Fisher <align.downpoured@gmail.com>
2018-03-20 21:57:23 -0700

    Writing more code

--- Vpc/initui.vpc.lua
+++ Vpc/initui.vpc.lua
@@ -1,34 +1,99 @@
 
 
---~ cd btn "glider_bg0"
---~ cd btn "glider_sprites1"
---~ cd btn "glider_sprites2"
---~ cd btn "glider_sprites3"
---~ cd btn "glider_sprites4"
---~ cd btn "glider_sprites5"
---~ cd btn "glider_sprites6"
---~ cd btn "glider_sprites7"
---~ cd btn "glider_sprites8"
---~ cd btn "glider_sprites9"
---~ cd btn "glider_sprites10"
---~ cd btn "glider_sprites11"
---~ cd btn "glider_sprites12"
---~ cd btn "glider_sprites13"
---~ cd btn "glider_sprites14"
---~ cd btn "glider_sprites15"
---~ cd btn "glider_sprites16"
---~ cd btn "glider_spritesme"
---~ cd btn "btn_continue"
+-- cd btn "glider_bg0"
+-- cd btn "glider_sprites1-16"
+-- cd btn "glider_spritesme"
+-- cd btn "glider_spritesshadow"
+-- cd btn "btn_continue"
+-- cd fld "roomname"
+-- cd fld "score"
+-- cd fld "lives"
+-- cd fld "behindlives"
+-- cd fld "gameover"
+-- cd btn "glider_spriteslivesicon"
 
--- lvl -1 is home
--- lvl -2 is tutorial1
--- lvl -3 is victory screen
+on mouseup
+    global curlevel, state
+    if curlevel is "" then
+        initSpriteConstantsAndLoadGameData
+        initui
+        put -1 into curlevel
+        put "nogame" into state
+        refreshOnLevelChange
+    end if
+    if the short name of the target is "btn_continue" then
+        if curlevel is "" or curlevel is -1 then
+            initSpriteConstantsAndLoadGameData
+            initui
+            put "nogame" into state
+            put -2 into curlevel
+            refreshOnLevelChange
+        else if curlevel is -2 then
+            startnewgame
+            refreshOnLevelChange
+        else if curlevel is -3 then
+            put "nogame" into state
+            put -1 into curlevel
+            refreshOnLevelChange
+        end if
+    end if
+end if
+
+on startnewgame
+    global cheat_invincible, state, curlevel, sprites_right_forward, sprites_shadoRght
+    global lastdirpressed, dy
+    initSpriteConstantsAndLoadGameData
+    put false into cheat_invincible
+    put "playing" into state
+    put 3 into cd fld "lives"
+    put 0 into cd fld "score"
+    put 1 into curlevel
+    set the icon of cd btn "glider_spritesme" to sprites_right_forward
+    set the icon of cd btn "glider_spritesshadow" to sprites_shadoRght
+    set the topleft of cd btn "glider_spritesme" to 20, 20
+    set the topleft of cd btn "glider_spritesshadow" to -400, -400
+    put "" into lastdirpressed
+    put 0 into dy
+end startnewgame
 
 on initui
-    global curlevel
+    global curlevel, sprites_right_forward
     set the style of cd btn "glider_bg0" to "transparent"
     set the showlabel of cd btn "glider_bg0" to false
     set the autohilite of cd btn "glider_bg0" to false
+    set the style of cd fld "roomname" to "shadow"
+    set the style of cd fld "score" to "shadow"
+    set the style of cd fld "behindlives" to "shadow"
+    set the style of cd fld "lives" to "transparent"
+    set the style of cd btn "glider_spriteslivesicon" to "transparent"
+    set the style of cd fld "gameover" to "shadow"
+    set the defaulttextfont of cd fld "gameover" to "times"
+    set the defaulttextsize of cd fld "gameover" to "18"
+    set the textalign of cd fld "gameover" to "center"
+    
+    set the style of cd btn "glider_bg0" to "transparent"
+    set the style of cd btn "glider_spritesme" to "transparent"
+    set the style of cd btn "glider_spritesshadow" to "transparent"
+    set the rect of cd btn "glider_bg0" to -1, -1, 515, 345
+    set the rect of cd btn "glider_spritesme" to 0,0,48,20
+    set the rect of cd btn "glider_spritesshadow" to 0,0,48,20
+    
+    put 20 into top
+    put 22 into h
+    set the rect of cd fld "roomname" to 28-26, top-18, 177, h
+    set the rect of cd fld "score" to 245-26, top-18, (245-26)+78, h
+    set the rect of cd fld "behindlives" to 328-26, top-18, (328-26)+207, h
+    set the rect of cd fld "lives" to 395-26, top-18, (395-26)+22, h
+    set the rect of cd btn "glider_spriteslivesicon" to 419-26, top-18, (419-26)+42, h
+    set the rect of cd fld "gameover" to 23,23,23+451,23+236
+    set the icon of cd btn "glider_spriteslivesicon" to sprites_right_forward
+    set the showlabel of cd btn "glider_spriteslivesicon" to false
+    
+    set the locktext of cd fld "roomname" to true 
+    set the locktext of cd fld "score" to true 
+    set the locktext of cd fld "lives" to true 
+    set the locktext of cd fld "behindlives" to true 
+    set the locktext of cd fld "gameover" to true 
     
     repeat with x=1 to 16
         hide cd btn ("glider_sprites" & x)
@@ -42,3 +107,6 @@ on initui
     put -1 into curlevel
     refreshOnLevelChange
 end initui
+
+
+
--- Vpc/mainloop.vpc.lua
+++ Vpc/mainloop.vpc.lua
@@ -12,8 +12,6 @@ function intersectRoomObject curlvldata, curlvlObjects, myrect1, myrect2, myrect
         put (propsperobj * i) + 2 into j
         put rectIntersect(myrect1, myrect2, myrect3, myrect4, item j of curlvlObjects,item j+1 of curlvlObjects,item j+2 of curlvlObjects, item j+3 of curlvlObjects) into intersected
         if intersected != "0" then
-            -- put (propsperobj * i) + 0 into j
-            --put "|" i & "|" & item j of curlvlObjects & "|" & item j+6 of curlvlObjects & "|"& item j+7 of curlvlObjects & "|"& item j+8 of curlvlObjects & "|" after result
             put "|" & i after result
         end if
     end repeat
@@ -25,25 +23,116 @@ function intersectRoomObject curlvldata, curlvlObjects, myrect1, myrect2, myrect
 end intersectRoomObject
 
 
-on initmainloop
-    global momentumx, momentumy
-    global lastdirpressed, propsperobj
-    put "" into lastdirpressed
-    put 0 into momentumx
-    put 0 into momentumy
-    put 9 into propsperobj
-    set the itemdel to "|"
-    repeat with x = 1 to 16
-        hide cd btn ("glider_sprite" & x)
+
+
+on mainloopgame_motion
+    global lastdirpressed, curlevel, lvlData
+    global dy
+    if dy is "" then
+        put 0 into dy
+    end if
+    if lastdirpressed is "-1" then
+        put -5 into dx
+    else if lastdirpressed is "1" then
+        put 5 into dx
+    end if
+    set the topleft of cd btn "glider_spritesme" to (the left of cd btn "glider_spritesme" + dx), (the top of cd btn "glider_spritesme" + dy)
+end mainloopgame_motion
+
+on mainloopgame_collisions
+    global dy
+    put line curlevel of lvlData into curlvldata
+    put line curlevel of lvlObjects into curlvlObjects
+    
+    put item 3 of curlvldata into leftopen
+    put item 4 of curlvldata into rghtopen
+    put intersectRoomObject(curlvldata, curlvlObjects, the left of cd btn "glider_spritesme", the top of cd btn "glider_spritesme", the right of cd btn "glider_spritesme", the bottom of cd btn "glider_spritesme") into intersects
+    put the number of items in intersects into numintersects
+    put false into isdead
+    repeat with i = 1 to numintersects
+        getCollideResult objtypename, isOn, amount, extra
     end repeat
-    updateLevel
-end initmainloop
+    
+    if newx < 0 then
+        if not isdead and leftopen != "0"
+            put curlevel - 1 into 
+        end if
+    end if
+end mainloopgame_collisions
+
+on mainloopgame
+    global curlevel, lvlData, dy
+    -- ideally, the hit box would be bigger to account for case when you are moving really fast
+    -- and could warp through a solid object
+    -- but we never go that fast, don't need it yet
+    mainloopgame_collisions
+    mainloopgame_motion
+end mainloopgame
+
+on mainloopdying
+    global deathcount, state, curlevel
+    if deathcount < 400 then
+        put deathcount+1 into deathcount
+    else
+        subtract 1 from cd fld "lives"
+        if cd fld "lives" < 0 then
+            put "nogame" into state
+            put -3 into curlevel
+            refreshOnLevelChange
+        else
+            set the topleft of cd btn "glider_spritesme" to 20,20
+            set the icon of cd btn "glider_spritesme" to sprites_right_forward
+            put "playing" into state
+        end if
+    end if
+end mainloopdying
+
+
+on mainloop
+    global state
+    if state is "playing" then
+        mainloopgame
+    else if state is "dying" then
+        mainloopdying
+    end if
+end mainloop
+
+on idle
+    mainloop
+end idle
+
+on afterkeydown
+    global lastdirpressed, cheat_invincible
+    if keychar() is "ArrowLeft" then
+        put "-1" into lastdirpressed
+    else if keychar() is "ArrowLeft" then
+        put "1" into lastdirpressed
+    else if keyChar() is "I" and shiftKey() then
+        answer "toggle cheatcode:invincible"
+    end if
+end afterkeydown
+
 
 on refreshOnLevelChange
     global curlevel
     repeat with x=1 to 16
         hide cd btn ("glider_sprites" & x)
     end repeat
+    hide cd btn "glider_spritesme"
+    hide cd btn "glider_spritesshadow"
+    
+    global lastdirpressed, propsperobj
+    put "" into lastdirpressed
+    put 9 into propsperobj
+    set the itemdel to "|"
+    
+    hide cd fld "roomname"
+    hide cd fld "score"
+    hide cd fld "lives"
+    hide cd fld "behindlives"
+    hide cd btn "glider_spriteslivesicon"
+    hide cd fld "gameover"
+    
     show cd btn "btn_continue"
     if curlevel is "" or curlevel is -1 then
         put -1 into curlevel
@@ -58,50 +147,21 @@ on refreshOnLevelChange
         set the icon of cd btn "glider_bg0" to 3
         set the rect of cd btn "btn_continue" to 126,300,126+254,300+36
         set the label of cd btn "btn_continue" to "Back"
+        if cd fld "lives" > 0 then
+            hide cd fld "gameover"
+        else
+            show cd fld "gameover"
+            put "Game Over..." & newline & newline & "Your score was "& (cd fld "score") & "." into fld "gameover"
+        end if
     else
+        show cd btn "glider_spritesme"
+        show cd btn "glider_spritesshadow"
+        show cd fld "roomname"
+        show cd fld "score"
+        show cd fld "lives"
+        show cd fld "behindlives"
+        hide cd btn "glider_spriteslivesicon"
         set the icon of cd btn "glider_bg0" to 3+curlevel
         hide cd btn "btn_continue"
-        
     end if
 end refreshOnLevelChange
-
-function mainloop
-    global momentumx, momentumy
-    global lastdirpressed, curlevel, lvlData
-    -- ideally, the hit box would be bigger to account for case when you are moving really fast
-    -- and could warp through a solid object
-    -- but we never go that fast, don't need it yet
-    if lastdirpressed is "-1" then
-        put -5 into dx
-    else if lastdirpressed is "1" then
-        put 5 into dx
-    end if
-    put line curlevel of lvlData into curlvldata
-    put line curlevel of lvlObjects into curlvlObjects
-    
-    put item 3 of curlvldata into leftopen
-    put item 4 of curlvldata into rghtopen
-    put intersectRoomObject(curlvldata, curlvlObjects, the left of cd btn "glider", the top of cd btn "glider", the right of cd btn "glider", the bottom of cd btn "glider") into intersects
-    put the number of items in intersects into numintersects
-    put false into isdead
-    repeat with i = 1 to numintersects
-        getCollideResult objtypename, isOn, amount, extra
-    end repeat
-    
-    if newx < 0 then
-        if not isdead and leftopen != "0"
-            put curlevel - 1 into 
-        end if
-    end if
-    
-end mainloop
-
-on afterkeydown
-    global lastdirpressed
-    if keychar() is "ArrowLeft" then
-        put "-1" into lastdirpressed
-    else if keychar() is "ArrowLeft" then
-        put "1" into lastdirpressed
-    end if
-end afterkeydown
-
